a59e93004df371496c4081c68adff43142540fe6
Haneul Choi <caelum02@snu.ac.kr>
2023-10-22 06:13:32 +0000

    Add preprocess.get_unit_resource

--- preprocess.py
+++ preprocess.py
@@ -13,16 +13,18 @@ from utils import replay_run_early_phase, replay_run_n_late_game_step
 
 MAP_SIZE = 64
 
-@vmap
+# First vectorize on the feature axis, and then on the team axis
+@partial(vmap, in_axes=0, out_axes=0) # team axis
+@partial(vmap, in_axes=(None, None, -1), out_axes=-1) # flax follows channels-last convention
 def to_board(x, y, unit_info):
     '''
     n_info: number of features to embed in the board (vectorized axis)
 
-    unit_info: ShapedArray(int8[n_info, MAX_N_UNITS])
-    x: ShapedArray(int8[n_info, MAX_N_UNITS])
-    y: ShapedArray(int8[n_info, MAX_N_UNITS])
+    unit_info: ShapedArray(int8[2, MAX_N_UNITS, n_info])
+    x: ShapedArray(int8[2, MAX_N_UNITS])
+    y: ShapedArray(int8[2, MAX_N_UNITS])
    
-    out: ShapedArray(int8[n_info, MAP_SIZE, MAP_SIZE])
+    out: ShapedArray(int8[2, MAP_SIZE, MAP_SIZE, n_info])
     '''
 
     zeros = jnp.zeros((MAP_SIZE, MAP_SIZE))
@@ -40,23 +42,39 @@ def get_unit_existence(unit_mask, unit_type, x, y):
         x : ShapedArray(int8[2, MAX_N_UNITS])
         y : ShapedArray(int8[2, MAX_N_UNITS])
 
-        output: ShapedArray(int8[4, MAX_N_UNITS])
+        output: ShapedArray(int8[2, MAP_SIZE, MAP_SIZE, 2]) 
 
-        light player 0, light player 1, heavy player 0, heavy player 1
-
-
-                unit type goes to axis 0 to preserve locality of player & unit_id axis
+        feature: [light, heavy]   
     '''
 
     light_mask = unit_mask & (unit_type==UnitType.LIGHT)
     heavy_mask = unit_mask & (unit_type==UnitType.HEAVY)
 
-    light_unit_map = to_board(x, y, light_mask)
-    heavy_unit_map = to_board(x, y, heavy_mask)
-
-    unit_map = jnp.concatenate((light_unit_map, heavy_unit_map))
+    mask = jnp.stack((light_mask, heavy_mask), axis=-1)
+    unit_map = to_board(x, y, mask)  
     
     return unit_map
+
+@jit
+def get_unit_resource(cargo, power, x, y,):
+    '''
+        unit_mask : ShapedArray(bool[2, MAX_N_UNITS])
+        cargo: ShapedArray(int32[2, MAX_N_UNITS, 4])
+        power: ShapedArray(int32[2, MAX_N_UNITS])
+        x : ShapedArray(int8[2, MAX_N_UNITS])
+        y : ShapedArray(int8[2, MAX_N_UNITS])
+
+        output: ShapedArray(int8[2, MAP_SIZE, MAP_SIZE, 5])
+
+        feature: [ice, ore, water, metal, power]
+    ''' 
+
+    resource = jnp.concatenate((cargo, power[...,None]), axis=-1)  
+
+    unit_resource_map = to_board(x, y, resource)
+
+    return unit_resource_map
+
 
 if __name__=="__main__":
 
