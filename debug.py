c331488591406ca5266ee3072d215851b409d1a5
Haneul Choi <caelum02@snu.ac.kr>
2023-10-23 10:45:11 +0000

    Minor change

--- debug.py
+++ debug.py
@@ -7,7 +7,9 @@ from utils import replay_run_early_phase, replay_run_n_late_game_step
 MAX_N_UNITS = 200
 n = 376
 
-lux_env, lux_actions = jux.utils.load_replay('https://www.kaggleusercontent.com/episodes/52900827.json')
+# replay_id = 52900827
+replay_id = 52958192
+lux_env, lux_actions = jux.utils.load_replay(f'replays/{replay_id}.json')
 jux_env, state = JuxEnv.from_lux(lux_env, buf_cfg=JuxBufferConfig(MAX_N_UNITS=MAX_N_UNITS))
 
 state, lux_actions = replay_run_early_phase(jux_env, state, lux_actions)
