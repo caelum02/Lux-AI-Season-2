250ea73833c65102c8e1d1072d3277a21c84f3de
Haneul Choi <caelum02@snu.ac.kr>
2023-11-08 09:42:01 +0000

    Add space.py

---
+++ src/space.py
@@ -0,0 +1,18 @@
+from typing import NamedTuple
+
+from jax import Array
+
+from jux.actions import JuxAction
+
+
+class ObsSpace(NamedTuple): # Input to the model
+    board_like_feature: Array
+    vector_feature: Array
+
+
+# TODO
+class ActionSpace(NamedTuple): # Output of the model
+    pass
+
+def act_space_to_jux_action(act_space: ActionSpace)->JuxAction:
+    pass
